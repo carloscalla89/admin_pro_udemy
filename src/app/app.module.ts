0ec8fde329909e89a587fa0b60f176c6b9bf53e2
carlos calla <carlos.calla89@gmail.com>
2018-09-29 16:38:14 -0500

    new changes

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,14 +1,28 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 
+//modulos
+import { PagesModule } from './pages/pages.module';
+
+//components
 import { AppComponent } from './app.component';
+import { LoginComponent } from './login/login.component';
+import { RegisterComponent } from './login/register.component';
+
+// Rutas
+import { APP_ROUTES } from './app.route';
+
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    LoginComponent,
+    RegisterComponent
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    APP_ROUTES,
+    PagesModule
   ],
   providers: [],
   bootstrap: [AppComponent]
